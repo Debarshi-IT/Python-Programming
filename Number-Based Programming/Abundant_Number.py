f99bb912e07b900238107f64fab29c31c29cad5b
Debarshi Chatterjee <debarshi.it.aec@gmail.com>
2025-05-12 00:05:45 +0530

    Add files via upload

--- Number-Based Programming/Abundant_Number.py
+++ Number-Based Programming/Abundant_Number.py
@@ -1,1 +1,7 @@
+inp=int(input("Enter your number here: "))
+temp=inp
 
+if sum(i for i in range(1, inp) if inp % i == 0) > inp:
+    print("Abundant number")
+else:
+    print("Not Abundant number")
