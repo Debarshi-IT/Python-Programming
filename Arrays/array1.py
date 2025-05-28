3162edfc9a134307cb9ee7368d0b402639305d0a
Debarshi Chatterjee <debarshi.it.aec@gmail.com>
2025-05-28 13:04:38 +0530

    Add files via upload

--- Arrays/array1.py
+++ Arrays/array1.py
@@ -1,1 +1,12 @@
+def main():
+    marks = []
+    print("Enter all marks: ")
+    for _ in range(10):
+        marks.append(int(input()))
 
+    print("The all marks:")
+    for mark in marks:
+        print(mark)
+
+if __name__ == "__main__":
+    main()
